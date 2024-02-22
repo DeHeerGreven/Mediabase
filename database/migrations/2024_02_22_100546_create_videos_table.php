eabd3b1e2dee064ec3931c11c05b524054cb2028
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2024-02-22 11:43:18 +0100

    Migrations klaar gemaakt
    Na het pullen php artisan migrate doen.
    
    yes kiezen als er nog geen is aangemaakt

--- database/migrations/2024_02_22_100546_create_videos_table.php
+++ database/migrations/2024_02_22_100546_create_videos_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('videos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('video_path');
             $table->foreignId('project_id')->nullable()->references('id')->on('projects');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('videos');
     }
 };
--- database/migrations/2024_02_22_103602_create_color_cards_table.php
+++ database/migrations/2024_02_22_103602_create_color_cards_table.php
@@ -11,14 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('color_cards', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->foreignId('project_id')->nullable()->references('id')->on('projects');
-            $table->rememberToken();
+            $table->string('hexcode');
+            $table->foreignId('moodboard_id')->nullable()->references('id')->on('moodboards');
             $table->timestamps();
         });
     }
@@ -28,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('color_cards');
     }
 };
