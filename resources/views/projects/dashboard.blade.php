56e6f2c1de6578b63b53904ed8d9d9ef785c201e
wesleysoeliman <wesley.soeliman@gmail.com>
2024-02-23 11:47:00 +0100

    Dashboard Styling + Desc

--- resources/views/projects/dashboard.blade.php
+++ resources/views/projects/dashboard.blade.php
@@ -1,22 +1,24 @@
-<x-app-layout>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-    <div class="container mx-auto">
-        <div class="mb-4 flex justify-between items-center">
-            <button class="bg-blue-500 text-white px-4 py-2 rounded mt-3">Maak een nieuw project</button>
-        </div>
+@extends('layouts.app')
 
-        <div class="grid grid-cols-3 gap-4">
-            @foreach ($projects as $project)
-            <div class="bg-white p-4 rounded shadow-md">
-                <h1>{{$project->name}}</h1>     
-            </div>
-            @endforeach
+@section('content')
+
+@if(session('success'))
+<div class="alert alert-success">
+    {{ session('success') }}
+</div>
+@endif
+
+<!-- Content -->
+<div class="container mx-auto">
+    <button class="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded mt-8">Maak een nieuw project</button>
+    <div class="grid grid-cols-3 gap-4 mt-8"> <!-- Added mt-8 class to add margin-top -->
+        @foreach ($projects as $project)
+        <div class="bg-white p-4 rounded shadow-md">
+            <h1>{{$project->name}}</h1> 
+            <p>{{$project->description}}</p>     
         </div>
+        @endforeach
     </div>
-    
-</x-app-layout>
+</div>
+
+@endsection
