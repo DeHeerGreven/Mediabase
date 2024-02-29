f866085c3e8756f3cb65e1c369d459da25586be5
DeHeerGreven <93603732+DeHeerGreven@users.noreply.github.com>
2024-02-29 12:14:28 +0100

    styling fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,26 +3,21 @@
 <head>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
 </head>
 <body>
 
-    <body style="display: flex; flex-direction: column; min-height: 100vh; margin: 0;">
+    <body>
 
         @include('components.header')
     
-        <div class="container">
             @yield('content')
-        </div>
-
+    
         @include('components.footer')
 
     <!-- Scripts -->
